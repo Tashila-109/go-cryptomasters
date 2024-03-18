d044323b320d7eaf3ea7f470eedc33433a50dbdf
Tashila Fernando <tashila.dev@gmail.com>
2024-03-18 14:53:20 +0530

    chore: get currency data

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"cryptomasters/go/crypto/api"
 	"fmt"
+	"time"
 )
 
 func main() {
-	rate, err := api.GetRate("BTC")
+	go getCurrencyData("BTC")
+	go getCurrencyData("ETH")
+	go getCurrencyData("BCH")
+	time.Sleep(time.Second * 5)
+}
+
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency)
 
 	if err == nil {
 		fmt.Printf("The rate for %v is %.2f \n", rate.Currency, rate.Price)
